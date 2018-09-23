38ca60451d6dabfecb4812907082d3ed5903f6d5
Wendell Antildes <wendellantildes@gmail.com>
2018-09-23 20:13:04 -0300

    Namespace updated

--- JsonValidatorExtensionsExample.Web.NetCore/Controllers/ExampleController.cs
+++ JsonValidatorExtensionsExample.Web.NetCore/Controllers/ExampleController.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using JsonValidatorExtensionsExample.Web.NetCore;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace JsonNumberNormalizer.Controllers
+namespace JsonValidatorExtensionsExample.Web.NetCore.Controllers
 {
     [Route("api/[controller]")]
     public class ExampleController : Controller
