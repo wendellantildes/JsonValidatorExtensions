056ca1064586dc5f7e774ff2cff6ee1ef5079dfd
Wendell Antildes <wendellantildes@gmail.com>
2018-09-23 20:10:56 -0300

    Middleware update in order to correct bug that did not serialize json objects.

--- JsonValidatorExtensions/JsonValidatorMiddleware.cs
+++ JsonValidatorExtensions/JsonValidatorMiddleware.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
 using Newtonsoft.Json;
 
 namespace JsonValidatorExtensions
@@ -22,27 +23,20 @@ namespace JsonValidatorExtensions
         public async Task InvokeAsync(HttpContext context)
         {
             var request = context.Request;
+            var body = request.Body;
             if (context.Request.ContentType != null && context.Request.ContentType == _jsonContentType)
             {
                 if (context.Request.Body != null)
                 {
                     request.EnableBuffering();
-
-                    var bodyStr = "";
-                    using (StreamReader reader
-                           = new StreamReader(request.Body))
+                    var bodyAsText = new StreamReader(context.Request.Body).ReadToEnd();
+                    context.Request.Body.Position = 0;
+                    if (!JsonValidatorTool.JsonValidator.IsValid(bodyAsText))
                     {
-                        bodyStr = reader.ReadToEnd();
-
-                    }
-                    if (!JsonValidatorTool.JsonValidator.IsValid(bodyStr))
-                    {
-                        
                         context.Response.StatusCode = _options.HttpStatusCode;
                         await context.Response.WriteAsync(JsonConvert.SerializeObject(_options.Data));
                         return;
                     }
-                    context.Request.Body.Position = 0;
                 }
             }
             await _next(context);
